6d9d840cf2c47e88ee23efe7aee2784f8ea78b73
Steven Vo <findme@stevenvo.info>
2015-11-09 23:41:46 -0800

    Initial Commit

---
+++ Smart-Arm-Band/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  Smart-Arm-Band
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project Smart-Arm-Band
+  Created by Steven Vo on 11/9/15
+  Copyright © 2015 Steven Vo
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2015
+  All rights reserved
+  http://embedXcode.weebly.com
+
---
+++ Smart-Arm-Band/Smart_Arm_Band.ino
@@ -0,0 +1,68 @@
+//
+// Smart-Arm-Band
+//
+// Description of the project
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Steven Vo
+// 				Steven Vo
+//
+// Date			11/9/15 11:41 PM
+// Version		<#version#>
+//
+// Copyright	© Steven Vo, 2015
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(WIRING) // Wiring specific
+#   include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+#   include "WProgram.h"
+#elif defined(MPIDE) // chipKIT specific
+#   include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+#   include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad specific
+#   include "Energia.h"
+#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
+#   include "LRF.h"
+#elif defined(MICRODUINO) // Microduino specific
+#   include "Arduino.h"
+#elif defined(SPARK) || defined(PARTICLE) // Particle / Spark specific
+#   include "Arduino.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#   include "Arduino.h"
+#elif defined(REDBEARLAB) // RedBearLab specific
+#   include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#   include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#   include "Arduino.h"
+#else // error
+#   error Platform not defined
+#endif // end IDE
+
+// Include application, user and local libraries
+
+
+// Prototypes
+
+
+// Define variables and constants
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
